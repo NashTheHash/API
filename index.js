7319b40d664cb2768dc8ee8a4ffcd07e074ddacc
NashTheHash <2554397@students.wits.ac.za>
2024-04-10 14:57:46 +0200

    Trying again

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/cars', (req, res) => {
     res.send("Hello, world!")
 });
+
 /*app.use(express.json());
 const cars = require('./cars.json');
 
